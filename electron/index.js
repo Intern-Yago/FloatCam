d64d44a18c6fea772199f3f9eabddbb4deeecdb2
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-11-20 10:53:05 -0300

    add logo | modify config camera

--- electron/index.js
+++ electron/index.js
@@ -1,17 +1,21 @@
 const {app, BrowserWindow, Tray} = require("electron")
 
 let win
+let width = 300
+let height = 300
 
 app.whenReady().then(function(){
     console.log(__dirname)
     
     win = new BrowserWindow({
-        icon: __dirname+"/assets/camera-fotografica.png",
-        width: 456,
-        height:264,
+        icon: __dirname+"/logo.png",
+        width: width,
+        height:height,
         frame:false,
+        alwaysOnTop: true,
+        transparent: true,
     })
-    win.loadFile("views/index.html")
+    win.loadFile("index.html")
 })
 
 app.on('window-all-closed', () => {
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 navigator.mediaDevices.getUserMedia({video:true}).then(function(mediaStream){
     var video = document.querySelector("#video")
     video.srcObject = mediaStream
-    video.play()
 }).catch(function (err){
     console.log("Não tem permissão");
 })
